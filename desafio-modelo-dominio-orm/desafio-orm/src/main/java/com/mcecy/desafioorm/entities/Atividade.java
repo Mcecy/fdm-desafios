f04041774780522cef7663ad30040cbdb1761599
mcecy <ceciliacadengue@gmail.com>
2022-12-14 17:48:28 -0300

    Adicionando desafio modelo de dominio e orm

---
+++ desafio-modelo-dominio-orm/desafio-orm/src/main/java/com/mcecy/desafioorm/entities/Atividade.java
@@ -0,0 +1,80 @@
+package com.mcecy.desafioorm.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.Set;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private Set<Bloco> blocos = new HashSet<>();
+
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
+
+    public Atividade() {
+    }
+
+    public Atividade(Integer id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
---
+++ desafio-modelo-dominio-orm/desafio-orm/src/test/java/com/mcecy/desafioorm/DesafioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcecy.desafioorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
